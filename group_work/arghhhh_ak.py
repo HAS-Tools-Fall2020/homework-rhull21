46d9b200be540cc1ff0e47952803765b820cc44b
Abigail <akahler@email.arizona.edu>
2020-11-07 11:01:20 -0700

    VS code copied to Colab copied to VS code test

---
+++ group_work/arghhhh_ak.py
@@ -0,0 +1,25 @@
+# Colab and Jupyter change formatting and the import names
+# I DON'T KNOW WHY THE FIRST IMPORT SAYS "import you"
+# I DON'T HAVE A CURSOR IN COLAB
+
+# Five hours and counting: Friday: 3hrs environment fixes, with help
+# Saturday: two hours environment fixing and Colab exploring
+
+# This is a test to see if more things change between saving this
+# as .py and pushing in kraken
+
+# %%
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+import datetime
+import json 
+import urllib.request as req
+import urllib
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
